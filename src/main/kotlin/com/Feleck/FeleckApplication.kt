e5556af9e80db25f3c3a57ea3746c53aedc39066
taehun2767 <taehun.lee@zep.us>
2024-04-15 22:08:05 +0900

    feat : 프로젝트 세팅

---
+++ src/main/kotlin/com/Feleck/FeleckApplication.kt
@@ -0,0 +1,11 @@
+package com.Feleck
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeleckApplication
+
+fun main(args: Array<String>) {
+	runApplication<FeleckApplication>(*args)
+}
---
+++ src/test/kotlin/com/Feleck/FeleckApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Feleck
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeleckApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
